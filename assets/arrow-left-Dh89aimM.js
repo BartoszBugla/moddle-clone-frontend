2d299769c0f784983f364d8e424eaa8cd28a6ecd
BartoszBugla <BartoszBugla@users.noreply.github.com>
2024-04-09 07:06:21 +0000

    deploy: 325986a49d6f6e31a6323da0bee0c101d7c1375e

--- assets/arrow-left-Dh89aimM.js
+++ assets/arrow-left-Dh89aimM.js
@@ -1,4 +1,4 @@
-import{ad as e}from"./index-HT7ANgvP.js";/**
+import{ad as e}from"./index-Bjj7LueE.js";/**
  * @license lucide-react v0.363.0 - ISC
  *
  * This source code is licensed under the ISC license.
