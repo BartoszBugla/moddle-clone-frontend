d899738990e6c1c6c95289978e6ffea3c4df8bbb
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2024-04-09 08:59:33 +0200

    fix: github pages routing

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,12 +1,17 @@
 // Import the generated route tree
-import { createRouter } from '@tanstack/react-router'
+import { createMemoryHistory, createRouter } from '@tanstack/react-router'
 
 import { routeTree } from '@/routeTree.gen'
+
+const memoryHistory = createMemoryHistory({
+  initialEntries: ['/'],
+})
 
 // Create a new router instance
 export const router = createRouter({
   routeTree,
   basepath: '/moddle-clone-frontend',
+  history: memoryHistory,
 })
 
 declare module '@tanstack/react-router' {
