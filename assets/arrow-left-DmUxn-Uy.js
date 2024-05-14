fc1ed247186ffcf035cba9126545f35aa5f8a5ea
BartoszBugla <BartoszBugla@users.noreply.github.com>
2024-05-14 19:26:00 +0000

    deploy: 2d8c0010b8de0191149fd73ad45f9c21cfcfc53c

--- assets/arrow-left-DmUxn-Uy.js
+++ assets/arrow-left-DmUxn-Uy.js
@@ -1,4 +1,4 @@
-import{I as e}from"./index-CRQIH0b2.js";/**
+import{A as e}from"./index-B_I-yeN2.js";/**
  * @license lucide-react v0.363.0 - ISC
  *
  * This source code is licensed under the ISC license.
